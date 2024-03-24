b637a0ec9c1ec7388a4473db89b3c9ff8b936b7c
pavanreddy21 <pavanreddygottimukkula@gmaail.com>
2024-03-24 11:23:59 -0700

    latest version

--- my-app/src/ChatUI/ChatUI.tsx
+++ my-app/src/ChatUI/ChatUI.tsx
@@ -1,10 +1,11 @@
 import { Flex } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ThreeDotsLoader from "./DotsLoader";
 import { colors } from "../theme";
 import { AnimatePresence } from "framer-motion";
 import Chat from "./Chat";
 import InputField from "./InputField";
+import MistralClient from "@mistralai/mistralai";
 
 type Chat = {
   user: "me" | "agent";
@@ -12,13 +13,20 @@ type Chat = {
   originalIndex: number;
 };
 
-const ChatUI = () => {
+const apiKey = "TWfVrlX659GSTS9hcsgUcPZ8uNzfoQsg"; // Ensure you have MISTRAL_API_KEY in your .env file
+const client = new MistralClient(apiKey);
+
+const PROMPT_TO_APPEND = ``;
+
+const ChatUI = ({ initialMessage }) => {
   const [chats, setChats] = useState<Chat[]>([]);
   const [value, setValue] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
+  // const [initialMessageSent, setInitialMessageSent] = useState(false);
+  const sentMessageRef = useRef(false);
 
-  const handleSubmit = async (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
+  const handleSubmit = async (e?: React.MouseEvent<HTMLElement>) => {
+    e?.preventDefault();
 
     try {
       setIsSubmitting(true);
@@ -27,31 +35,33 @@ const ChatUI = () => {
         { user: "me", message: value, originalIndex: prev.length },
         ...prev,
       ]);
+      // Prepare the conversation history for the Mistral chat API
+      const conversationHistory = chats
+        .map((chat) => ({
+          role: chat.user === "me" ? "user" : "assistant", // Adjust if needed based on your API's expected roles
+          content: chat.message,
+        }))
+        .reverse(); // Reverse to maintain the order of messages as they were sent
 
-      //   const response = await fetch("/api/generate", {
-      //     method: "POST",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //     },
-      //     body: JSON.stringify({
-      //       message: [{ user: "me", message: value }, ...chats]
-      //         .reverse()
-      //         .map((d) => d.message)
-      //         .join(""),
-      //     }),
-      //   });
+      // Include the new message by the user at the end of the conversation history
+      conversationHistory.push({ role: "user", content: value });
 
-      //   const data = await response.json();
-      //   if (response.status !== 200) {
-      //     throw (
-      //       data.error ||
-      //       new Error(`Request failed with status ${response.status}`)
-      //     );
-      //   }
-      setChats((prev) => [
-        { user: "agent", message: "test", originalIndex: prev.length },
-        ...prev,
-      ]);
+      const chatResponse = await client.chat({
+        model: "mistral-large-latest",
+        messages: conversationHistory,
+      });
+      if (chatResponse && chatResponse.choices.length > 0) {
+        // Update the chat with the response from the Mistral chat API
+        setChats((prev) => [
+          {
+            user: "agent",
+            message: chatResponse.choices[0].message.content,
+            originalIndex: prev.length,
+          },
+          ...prev,
+        ]);
+      }
+      setValue(""); // Clear the input after sending
     } catch (error) {
       console.log(error);
     } finally {
@@ -59,6 +69,22 @@ const ChatUI = () => {
     }
   };
 
+  useEffect(() => {
+    console.log("rendering twise");
+    // if (initialMessageSent) return;
+    // setInitialMessageSent(true);
+    if (sentMessageRef.current) return;
+    sentMessageRef.current = true;
+    const messageToSend = PROMPT_TO_APPEND + initialMessage;
+    setChats((prev) => [
+      { user: "me", message: messageToSend, originalIndex: prev.length },
+      ...prev,
+    ]);
+    setValue(messageToSend);
+    handleSubmit(null);
+  }, [initialMessage]);
+
+  console.log("chats", chats);
   return (
     <Flex bg="main-bg" h="90vh" color="main-color">
       <Flex
@@ -94,6 +120,9 @@ const ChatUI = () => {
           )}
           <AnimatePresence>
             {chats.map((chat, index) => {
+              if (chat.originalIndex === 0 && chat.user === "me") {
+                return null;
+              }
               return (
                 <Chat
                   key={chat.originalIndex}
--- my-app/src/Dashboard/Dashboard.tsx
+++ my-app/src/Dashboard/Dashboard.tsx
@@ -11,6 +11,7 @@ import {
 import FileUpload from "../FileUpload/FileUpload";
 import CustomFlameGraph from "../Flamegraph/FlameGraph";
 import Markdown from "react-markdown";
+import ChatUI from "../ChatUI/ChatUI";
 
 function Dashboard() {
   const [responseFromBE, setResponseFromBE] = useState(null);
@@ -84,127 +85,158 @@ function Dashboard() {
           <FileUpload handleFileUpload={handleFileUpload} />
         </Box>
       ) : (
-        <Flex direction="row" height="100%" textAlign="justify">
-          {responseFromBE.explanation_cards &&
-            responseFromBE.explanation_cards.length > 0 && (
+        <Flex direction="row" height="100%">
+          <Box
+            w="50%"
+            h="100%"
+            p={4}
+            bg={theme.colors.white}
+            borderRadius="md"
+            boxShadow="sm"
+          >
+            <Flex direction="column" height="100%" textAlign="justify">
+              {responseFromBE.explanation_cards &&
+                responseFromBE.explanation_cards.length > 0 && (
+                  <Box
+                    p={4}
+                    bg={theme.colors.white}
+                    borderRadius="md"
+                    boxShadow="sm"
+                    mb={4}
+                    width="50"
+                  >
+                    <Heading noOfLines={1}>
+                      {responseFromBE.explanation_cards[currentCardIndex].title}
+                    </Heading>
+                    <Box p="4">
+                      <Markdown
+                        components={{
+                          // Customize the root element to set the overall theme background and text color
+                          // root: {
+                          //   style: {
+                          //     backgroundColor: "#1e1e1e", // Dark background
+                          //     color: "#dcdcdc", // Light text
+                          //     fontFamily:
+                          //       "'Consolas', 'Monaco', 'Andale Mono', 'Ubuntu Mono', monospace",
+                          //     padding: "20px",
+                          //     borderRadius: "4px",
+                          //     lineHeight: "1.6",
+                          //   },
+                          // },
+                          // Customize headers, adjust sizes as needed
+                          h1: ({ node, ...props }) => (
+                            <h1 style={{ color: "#569cd6" }} {...props} />
+                          ),
+                          h2: ({ node, ...props }) => (
+                            <h2 style={{ color: "#4ec9b0" }} {...props} />
+                          ),
+                          // Customize paragraphs
+                          p: ({ node, ...props }) => (
+                            <p style={{ margin: "10px 0" }} {...props} />
+                          ),
+                          // Customize links
+                          a: ({ node, ...props }) => (
+                            <a style={{ color: "#9cdcfe" }} {...props} />
+                          ),
+                          // Customize code blocks
+                          code({ node, className, children, ...props }) {
+                            const match = /language-(\w+)/.exec(
+                              className || ""
+                            );
+                            return match ? (
+                              <pre
+                                style={{
+                                  background: "#1e1e1e",
+                                  padding: "10px",
+                                  borderRadius: "5px",
+                                }}
+                              >
+                                <code
+                                  style={{
+                                    fontFamily:
+                                      "'Consolas', 'Monaco', 'Andale Mono', 'Ubuntu Mono', monospace",
+                                    color: "#ce9178",
+                                  }}
+                                  {...props}
+                                >
+                                  {children}
+                                </code>
+                              </pre>
+                            ) : (
+                              <code
+                                style={{
+                                  fontFamily:
+                                    "'Consolas', 'Monaco', 'Andale Mono', 'Ubuntu Mono', monospace",
+                                  backgroundColor: "#2E3440",
+                                  color: "#D8DEE9",
+                                  padding: "0 2px",
+                                  margin: "4px",
+                                  borderRadius: "4px",
+                                }}
+                                {...props}
+                              >
+                                {children}
+                              </code>
+                            );
+                          },
+                          // Add other customizations for different elements as needed
+                        }}
+                      >
+                        {
+                          responseFromBE.explanation_cards[currentCardIndex]
+                            .content
+                        }
+                      </Markdown>
+                    </Box>
+                    <Flex mt={4} justify="space-between">
+                      <Button
+                        onClick={handlePrev}
+                        isDisabled={currentCardIndex === 0}
+                      >
+                        Previous
+                      </Button>
+                      <Button
+                        onClick={handleNext}
+                        isDisabled={
+                          currentCardIndex ===
+                          (responseFromBE.explanation_cards?.length ?? 0) - 1
+                        }
+                      >
+                        Next
+                      </Button>
+                    </Flex>
+                  </Box>
+                )}
+              <Divider orientation="horizontal" my={2} />
               <Box
                 p={4}
                 bg={theme.colors.white}
                 borderRadius="md"
                 boxShadow="sm"
-                mb={4}
                 width="50"
               >
-                <Heading noOfLines={1}>
-                  {responseFromBE.explanation_cards[currentCardIndex].title}
-                </Heading>
-                <Box p="4">
-                  <Markdown
-                    components={{
-                      // Customize the root element to set the overall theme background and text color
-                      // root: {
-                      //   style: {
-                      //     backgroundColor: "#1e1e1e", // Dark background
-                      //     color: "#dcdcdc", // Light text
-                      //     fontFamily:
-                      //       "'Consolas', 'Monaco', 'Andale Mono', 'Ubuntu Mono', monospace",
-                      //     padding: "20px",
-                      //     borderRadius: "4px",
-                      //     lineHeight: "1.6",
-                      //   },
-                      // },
-                      // Customize headers, adjust sizes as needed
-                      h1: ({ node, ...props }) => (
-                        <h1 style={{ color: "#569cd6" }} {...props} />
-                      ),
-                      h2: ({ node, ...props }) => (
-                        <h2 style={{ color: "#4ec9b0" }} {...props} />
-                      ),
-                      // Customize paragraphs
-                      p: ({ node, ...props }) => (
-                        <p style={{ margin: "10px 0" }} {...props} />
-                      ),
-                      // Customize links
-                      a: ({ node, ...props }) => (
-                        <a style={{ color: "#9cdcfe" }} {...props} />
-                      ),
-                      // Customize code blocks
-                      code({ node, className, children, ...props }) {
-                        const match = /language-(\w+)/.exec(className || "");
-                        return match ? (
-                          <pre
-                            style={{
-                              background: "#1e1e1e",
-                              padding: "10px",
-                              borderRadius: "5px",
-                            }}
-                          >
-                            <code
-                              style={{
-                                fontFamily:
-                                  "'Consolas', 'Monaco', 'Andale Mono', 'Ubuntu Mono', monospace",
-                                color: "#ce9178",
-                              }}
-                              {...props}
-                            >
-                              {children}
-                            </code>
-                          </pre>
-                        ) : (
-                          <code
-                            style={{
-                              fontFamily:
-                                "'Consolas', 'Monaco', 'Andale Mono', 'Ubuntu Mono', monospace",
-                              backgroundColor: "#2E3440",
-                              color: "#D8DEE9",
-                              padding: "0 2px",
-                              margin: "4px",
-                              borderRadius: "4px",
-                            }}
-                            {...props}
-                          >
-                            {children}
-                          </code>
-                        );
-                      },
-                      // Add other customizations for different elements as needed
-                    }}
-                  >
-                    {responseFromBE.explanation_cards[currentCardIndex].content}
-                  </Markdown>
-                </Box>
-                <Flex mt={4} justify="space-between">
-                  <Button
-                    onClick={handlePrev}
-                    isDisabled={currentCardIndex === 0}
-                  >
-                    Previous
-                  </Button>
-                  <Button
-                    onClick={handleNext}
-                    isDisabled={
-                      currentCardIndex ===
-                      (responseFromBE.explanation_cards?.length ?? 0) - 1
-                    }
-                  >
-                    Next
-                  </Button>
-                </Flex>
+                {responseFromBE.flame_graph && (
+                  <CustomFlameGraph
+                    graphData={JSON.parse(responseFromBE.flame_graph)}
+                  />
+                )}
               </Box>
-            )}
+            </Flex>
+          </Box>
           <Divider orientation="vertical" mx={2} />
           <Box
+            w="50%"
+            h="100%"
             p={4}
             bg={theme.colors.white}
             borderRadius="md"
             boxShadow="sm"
-            width="50"
           >
-            {responseFromBE.flame_graph && (
-              <CustomFlameGraph
-                graphData={JSON.parse(responseFromBE.flame_graph)}
-              />
-            )}
+            <ChatUI
+              initialMessage={
+                responseFromBE?.explanation_cards?.[currentCardIndex]?.content
+              }
+            />
           </Box>
         </Flex>
       )}
